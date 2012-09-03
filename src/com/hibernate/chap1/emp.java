d7631ed31feecb6f65a3291628754eb7c02762f7
meherzad <meherzad4u@gmail.com>
2012-09-03 19:38:41 +0530

    03/09

--- src/com/hibernate/chap1/emp.java
+++ src/com/hibernate/chap1/emp.java
@@ -1,14 +1,27 @@
 package com.hibernate.chap1;
 
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.TableGenerator;
 
 @Entity
 public class emp {
 	int empId;
 	String empName;
 	
+	/*
+	 * 
+	 * 
+	 * @TableGenerator(name="empid", table="empPKey", pkColumnName="empkey", pkColumnValue="empvalue",allocationSize=1)
+	@GeneratedValue(strategy=GenerationType.TABLE, generator="empid")
+	 */
+	
 	@Id
+	@GeneratedValue
 	public int getEmpId() {
 		return empId;
 	}
--- src/com/hibernate/chap1/testEmp.java
+++ src/com/hibernate/chap1/testEmp.java
@@ -1,7 +1,13 @@
 package com.hibernate.chap1;
 
 //import org.hibernate.cfg.AnnotationConfiguration;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
 import org.hibernate.tool.hbm2ddl.SchemaExport;
 
 public class testEmp {
@@ -17,10 +23,32 @@ public class testEmp {
 
 		Configuration config = new Configuration();
 		config.addAnnotatedClass(emp.class);
-		config.configure("hibernate.cfg.xml"); //  Also allowed 		config.configure();
+		config.configure("hibernate.cfg.xml"); // Also allowed
+												// config.configure();
+		ServiceRegistry serviceRegistry = new ServiceRegistryBuilder()
+				.applySettings(config.getProperties()).buildServiceRegistry();
 
-		new SchemaExport(config).create(true, true);
+		// new SchemaExport(config).create(true, true);//create new table
 
+		SessionFactory factory = config.buildSessionFactory(serviceRegistry); // Resource
+		// intensive so
+		// minimize use
+		Session session = factory.getCurrentSession();
+		session.beginTransaction();
+
+		Query query = (Query) session.createQuery("from emp where id=:id");
+
+		query.setParameter("id", 5);
+		System.out.println("out put -------->>>>>>>>   ");
+		emp objectRetrieve = (emp) query.uniqueResult();
+		System.out.println("out put -------->>>>>>>>   "
+				+ objectRetrieve.getEmpName());
+
+		/*
+		 * emp obj = new emp(); // Transient object // obj.setEmpId(10);
+		 * obj.setEmpName("Ale55x"); session.save(obj);
+		 */
+		session.getTransaction().commit();
 	}
 
 }
